9fe6e127c7ab0ba65bfd769565d98aa8c3769f72
Marian Amiragov <marian@eduroam-103-158.zdv.Uni-Mainz.DE>
2022-03-07 13:53:17 +0100

    Autostop on >95% accuracy

--- chapter2.py
+++ chapter2.py
@@ -3,6 +3,15 @@ import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 
+# Callback to stop on >95% accuracy
+class AccuracyAtLeast95(tf.keras.callbacks.Callback): # inherit from tf.keras.callbacks.Callback
+  def on_epoch_end(self, epoch, logs={}):
+    if (logs.get('accuracy')>.95):
+      print('\n☺️ Accuracy above 95%. Training will be stopped now.')
+      self.model.stop_training = True
+
+callbacks = AccuracyAtLeast95()
+
 # Get MNIST fashion data set
 data = tf.keras.datasets.fashion_mnist
 (training_images, training_labels), (test_images, test_labels) = data.load_data()
@@ -11,7 +20,7 @@ data = tf.keras.datasets.fashion_mnist
 training_images = training_images / 255.0
 test_images = test_images / 255.0
 
-# 2D Array to 1D Array then 
+# 2D Array to 1D Array then 2-layer dense network
 model = Sequential([
   Flatten(input_shape=(28, 28)),
   Dense(128, activation=tf.nn.relu), # activation: rectified linear unit -> y = max(0, x)
@@ -20,12 +29,12 @@ model = Sequential([
 
 model.compile(
   optimizer='adam', # adam is an evolution to stochastic gradient optimizer
-  loss='sparse_categorical_crossentropy', # 
+  loss='sparse_categorical_crossentropy', # good for categorization
   metrics=['accuracy']
 )
 
 # Training
-model.fit(training_images, training_labels, epochs=50)
+model.fit(training_images, training_labels, epochs=50, callbacks=[callbacks])
 
 # Testing
 model.evaluate(test_images, test_labels)
