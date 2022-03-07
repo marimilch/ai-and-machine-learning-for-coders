87eca2c090458fa227c4afd0b6f3ed719f3e2cef
Marian Amiragov <marian@eduroam-103-158.zdv.Uni-Mainz.DE>
2022-03-07 13:28:47 +0100

    milestone chapter 2

---
+++ chapter2.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+
+# Get MNIST fashion data set
+data = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = data.load_data()
+
+# Normalize images
+training_images = training_images / 255.0
+test_images = test_images / 255.0
+
+# 2D Array to 1D Array then 
+model = Sequential([
+  Flatten(input_shape=(28, 28)),
+  Dense(128, activation=tf.nn.relu), # activation: rectified linear unit -> y = max(0, x)
+  Dense(10, activation=tf.nn.softmax), # activation: softmax, typical for last layers
+])
+
+model.compile(
+  optimizer='adam', # adam is an evolution to stochastic gradient optimizer
+  loss='sparse_categorical_crossentropy', # 
+  metrics=['accuracy']
+)
+
+# Training
+model.fit(training_images, training_labels, epochs=50)
+
+# Testing
+model.evaluate(test_images, test_labels)
+
+# Prediction
+# classifications = model.predict(test_images)
+
+# print(classifications[0])
+# print(test_labels[0])
+
+
